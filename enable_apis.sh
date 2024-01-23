158e52fd4ce93b00437b07c1f64e07668d8fedc9
jrdetorre-google <114235341+jrdetorre-google@users.noreply.github.com>
2024-01-23 12:42:22 +0100

    upload enable apis

---
+++ enable_apis.sh
@@ -0,0 +1,47 @@
+gcloud services enable bigqueryconnection.googleapis.com
+gcloud services enable notebooks.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable aiplatform.googleapis.com
+gcloud services enable pubsub.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable dataflow.googleapis.com
+gcloud services enable bigquery.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
+gcloud services enable iam.googleapis.com
+
+
+# Run the following command to grant the Compute Engine default service account access to read and write pipeline artifacts in Google Cloud Storage.
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUM=$(gcloud projects list --filter="$PROJECT_ID" --format="value(PROJECT_NUMBER)")
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:${PROJECT_NUM}-compute@developer.gserviceaccount.com"\
+      --role='aiplatform.serviceAgent'
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:${PROJECT_NUM}-compute@developer.gserviceaccount.com"\
+      --role='roles/artifactregistry.admin'
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+        --member="serviceAccount:${PROJECT_NUM}-compute@developer.gserviceaccount.com" \
+        --role='roles/bigquery.connectionAdmin'
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:${PROJECT_NUM}-compute@developer.gserviceaccount.com"\
+      --role='roles/storage.admin'
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:${PROJECT_NUM}@cloudbuild.gserviceaccount.com"\
+      --role='roles/aiplatform.admin'
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:$PROJECT_NUM-compute@developer.gserviceaccount.com"\
+      --role='roles/run.admin'
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:$PROJECT_NUM-compute@developer.gserviceaccount.com"\
+      --role='roles/resourcemanager.projectIamAdmin'
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:service-${PROJECT_NUM}@gcp-sa-aiplatform.iam.gserviceaccount.com"\
+      --role='roles/artifactregistry.writer'
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member="serviceAccount:service-${PROJECT_NUM}@gcp-sa-aiplatform.iam.gserviceaccount.com"\
+      --role='roles/storage.objectAdmin' 
